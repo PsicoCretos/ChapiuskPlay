5faf6d13fa0417582b8c19f11299127084f0234b
Guilherme Martins <fellipini@gmail.com>
2022-12-20 16:10:01 -0300

    changes

---
+++ data.js
@@ -0,0 +1,20 @@
+window.audios = [
+  {
+    title: "WAIT FOR U", // titulo som
+    artist: "WAIT FOR U 2", // artista
+    cover: "Aftermath.jpg", // img
+    file: "WAIT FOR U (feat. Drake & Tems).mp3", // arquivo de audio
+  },
+  {
+    title: "Paint It,Black2",
+    artist: "The Rolling Stones2",
+    cover: "Aftermath2.jpg",
+    file: "aftermath2.mp3",
+  },
+  {
+    title: "Paint It,Black3",
+    artist: "The Rolling Stones3",
+    cover: "Aftermath3.jpg",
+    file: "aftermath3.mp3",
+  },
+];
